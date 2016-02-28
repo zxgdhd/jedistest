66ce3fdc16a87d6d75844bdf3d49cedb76fcb4d1
zxgdhd <872797959@qq.com>
2016-02-28 22:41:50 +0800

    ubuntu 完善测试用例，添加pipeline，transaction，sync（），exec（）的测试用例，添加获取jedis对象的方法

--- src/main/java/com/cssiot/redis/JedisTool.java
+++ src/main/java/com/cssiot/redis/JedisTool.java
@@ -1,6 +1,7 @@
 package com.cssiot.redis;
 
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
@@ -32,16 +33,58 @@ public class JedisTool {
 	}
 	
 	/**
-	 * @param config	JedisPoolConfig对象
-	 * @param host		redis服务器地址
-	 * @param port		redis服务器端口
-	 * @param timeout	获取链接的最长时间
-	 * @param auth		连接密码
-	 * @return			JedisPool对象
-	 * getJedisPool
-	 * JedisPool
-	 * woow
-	 * TODO		根据配置对象生成JedisTool对象
+	 * @param config
+	 *            JedisPoolConfig对象
+	 * 
+	 * 
+	 *            maxActive：控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；如果赋值为-
+	 *            1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted。
+	 * 
+	 *            maxIdle：控制一个pool最多有多少个状态为idle(空闲)的jedis实例；
+	 * 
+	 *            whenExhaustedAction：表示当pool中的jedis实例都被allocated完时，pool要采取的操作；
+	 *            默认有三种。 WHEN_EXHAUSTED_FAIL -->
+	 *            表示无jedis实例时，直接抛出NoSuchElementException； WHEN_EXHAUSTED_BLOCK
+	 *            --> 则表示阻塞住，或者达到maxWait时抛出JedisConnectionException；
+	 *            WHEN_EXHAUSTED_GROW --> 则表示新建一个jedis实例，也就说设置的maxActive无用；
+	 * 
+	 *            maxWait：表示当borrow一个jedis实例时，最大的等待时间，如果超过等待时间，
+	 *            则直接抛出JedisConnectionException；
+	 * 
+	 *            testOnBorrow：在borrow一个jedis实例时，是否提前进行alidate操作；如果为true，
+	 *            则得到的jedis实例均是可用的；
+	 * 
+	 *            testOnReturn：在return给pool时，是否提前进行validate操作；
+	 * 
+	 *            testWhileIdle：如果为true，表示有一个idle object evitor线程对idle
+	 *            object进行扫描，如果validate失败，此object会被从pool中drop掉；
+	 *            这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义；
+	 * 
+	 *            timeBetweenEvictionRunsMillis：表示idle object
+	 *            evitor两次扫描之间要sleep的毫秒数；
+	 * 
+	 *            numTestsPerEvictionRun：表示idle object evitor每次扫描的最多的对象数；
+	 * 
+	 *            minEvictableIdleTimeMillis：表示一个对象至少停留在idle状态的最短时间，然后才能被idle
+	 *            object
+	 *            evitor扫描并驱逐；这一项只有在timeBetweenEvictionRunsMillis大于0时才有意义；
+	 * 
+	 *            softMinEvictableIdleTimeMillis：在minEvictableIdleTimeMillis基础上，
+	 *            加入了至少minIdle个对象已经在pool里面了。如果为-1，evicted不会根据idle
+	 *            time驱逐任何对象。如果minEvictableIdleTimeMillis>0，则此项设置无意义，
+	 *            且只有在timeBetweenEvictionRunsMillis大于0时才有意义；
+	 * 
+	 *            lifo：borrowObject返回对象时，是采用DEFAULT_LIFO（last in first
+	 *            out，即类似cache的最频繁使用队列），如果为False，则表示FIFO队列；
+	 * @param host
+	 *            redis服务器地址
+	 * @param port
+	 *            redis服务器端口
+	 * @param timeout
+	 *            获取链接的最长时间
+	 * @param auth
+	 *            连接密码
+	 * @return JedisPool对象 getJedisPool JedisPool woow TODO 根据配置对象生成JedisTool对象
 	 */
 	public static JedisPool getJedisPool(JedisPoolConfig config,String host,int port,int timeout,String auth){
 		if(pool==null){
@@ -52,6 +95,30 @@ public class JedisTool {
 			}
 		}
 		return pool;
+	}
+	
+	
+	/**
+	 * @return		Jedis对象
+	 * getJedis
+	 * Jedis
+	 * woow
+	 * TODO		获取Jedis对象
+	 */
+	public static Jedis getJedis(){
+		return getJedisPool().getResource();
+	}
+	
+	
+	/**
+	 * @return		Jedis对象
+	 * getJedis
+	 * Jedis
+	 * woow
+	 * TODO		获取Jedis对象
+	 */
+	public static Jedis getJedis(JedisPoolConfig config,String host,int port,int timeout,String auth){
+		return getJedisPool(config,host,port,timeout,auth).getResource();
 	}
 
 }
--- src/test/java/com/cssiot/junit/JedisToolTest.java
+++ src/test/java/com/cssiot/junit/JedisToolTest.java
@@ -1,34 +1,141 @@
 package com.cssiot.junit;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import com.cssiot.redis.JedisTool;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Pipeline;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
 
 public class JedisToolTest {
-	
-	
+
+	/**
+	 * 
+	 * getJedisPool
+	 * void
+	 * woow
+	 * TODO		测试无参redis连接
+	 */
 	@Test
-	public void getJedisPool(){
-		JedisPool pool=JedisTool.getJedisPool();
-		if (pool!=null) {
+	public void getJedisPool() {
+		JedisPool pool = JedisTool.getJedisPool();
+		if (pool != null) {
 			System.out.println("获取JedisPool成功！");
 		}
 	}
-	
+
+	/**
+	 * 
+	 * getJedisPoolByConfig
+	 * void
+	 * woow
+	 * TODO		指定JedisPoolConfig、主机地址、端口号、超时时间、连接密码 获取jedispool
+	 */
 	@Test
-	public void getJedisPoolByConfig(){
-		JedisPoolConfig config=new JedisPoolConfig();
+	public void getJedisPoolByConfig() {
+		JedisPoolConfig config = new JedisPoolConfig();
 		config.setMaxIdle(10000);
 		config.setMaxTotal(2000);
 		config.setMaxWaitMillis(10000);
 		config.setTestOnBorrow(true);
-		JedisPool pool=JedisTool.getJedisPool(config,"127.0.0.1",6379,10000,null);
-		if (pool!=null) {
+		JedisPool pool = JedisTool.getJedisPool(config, "127.0.0.1", 6379, 10000, null);
+		if (pool != null) {
 			System.out.println("获取JedisPool成功！");
 		}
+	}
+	
+	
+	/**
+	 * 
+	 * jedisPipeLine: starts a pipeline,which is a very efficient way to send
+	 * lots of command and read all the responses when you finish sending them
+	 * 适合进行批量处理请求
+	 * 
+	 * void woow 
+	 * 
+	 * TODO 测试jedis pipeline
+	 */
+	@Test
+	public  void jedisPipeLine(){
+		JedisPool pool=JedisTool.getJedisPool();
+		Jedis jedis=pool.getResource();
+		Pipeline pipeline=jedis.pipelined();
+		pipeline.set("name", "oh I'm woow,my name is zxgdhd!");
+		Response<String> name=pipeline.get("name");
+		
+		/*
+		 * exec是在事务下使用的，执行之前需要开启使用multi开始事务，之后使用exec执行事务。所以这里没有使用multi开始事务的情况下，
+		 * 其实是不应该用exec的，否则会出现ERR EXEC without MULTI的返回
+		 */
+		//		pipeline.exec();
+		pipeline.sync();
+		try {
+			pipeline.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("The name is "+name.get());
+	}
+	
+	
+	
+	/**
+	 * 
+	 * jedisPipeLine: starts a pipeline,which is a very efficient way to send
+	 * lots of command and read all the responses when you finish sending them
+	 * 适合进行批量处理请求
+	 * 
+	 * void woow 
+	 * 
+	 * TODO 测试jedis pipeline exec()方法
+	 */
+	@Test
+	public  void jedisPipeLineSync(){
+		JedisPool pool=JedisTool.getJedisPool();
+		Jedis jedis=pool.getResource();
+		Pipeline pipeline=jedis.pipelined();
+		pipeline.set("name", "oh I'm woow,my name is zxgdhd!");
+		pipeline.append("name", "I'm coder!");
+		pipeline.append("name", "I'm working in CSSIOT!");
+		Response<String> name=pipeline.get("name");
+		
+		/*
+		 * exec是在事务下使用的，执行之前需要开启使用multi开始事务，之后使用exec执行事务。所以这里没有使用multi开始事务的情况下，
+		 * 其实是不应该用exec的
+		 */
+		pipeline.multi();
+		pipeline.exec();
+		try {
+			pipeline.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("The name is "+name.get());
+	}
+	
+	/**
+	 * 
+	 * testJedisTransaction
+	 * void
+	 * woow
+	 * TODO		测试Jedis Transaction
+	 */
+	@Test
+	public void testJedisTransaction(){
+		Jedis jedis=JedisTool.getJedis();
+		Transaction tx=jedis.multi();
+		for (int i = 0; i < 10; i++) {
+			tx.append("name", ","+i);
+		}
+		Response<String> name=tx.get("name");
+		tx.exec();
+		System.out.println(name.get());
 	}
 
 }
